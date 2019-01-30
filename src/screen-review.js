af8619f9c47779dd2a8e51dc309717e5d291da88
stbtpersonal <stbt.personal@gmail.com>
2019-01-30 16:06:01 +0200

    Implemented an English->Japanese "IME".

--- src/screen-review.js
+++ src/screen-review.js
@@ -4,6 +4,141 @@
     var ENTRY_TYPE_J_TO_E = "J_TO_E";
     var ENTRY_TYPE_E_TO_J = "E_TO_J";
 
+    var INPUT_TRANSFORMATION_DICTIONARY = {
+        "a": "あ",
+        "i": "い",
+        "u": "う",
+        "e": "え",
+        "o": "お",
+
+        "ka": "か",
+        "ki": "き",
+        "ku": "く",
+        "ke": "け",
+        "ko": "こ",
+
+        "sa": "さ",
+        "shi": "し",
+        "su": "す",
+        "se": "せ",
+        "so": "そ",
+
+        "ta": "た",
+        "chi": "ち",
+        "tsu": "つ",
+        "te": "て",
+        "to": "と",
+        
+        "na": "な",
+        "ni": "に",
+        "nu": "ぬ",
+        "ne": "ね",
+        "no": "の",
+        
+        "ha": "は",
+        "hi": "ひ",
+        "fu": "ふ",
+        "he": "へ",
+        "ho": "ほ",
+        
+        "ma": "ま",
+        "mi": "み",
+        "mu": "む",
+        "me": "め",
+        "mo": "も",
+        
+        "ya": "や",
+        "yu": "ゆ",
+        "yo": "よ",
+        
+        "ra": "ら",
+        "ri": "り",
+        "ru": "る",
+        "re": "れ",
+        "ro": "ろ",
+        
+        "wa": "わ",
+        "wo": "を",
+        "nn": "ん",
+
+        "ga": "が",
+        "gi": "ぎ",
+        "gu": "ぐ",
+        "ge": "げ",
+        "go": "ご",
+
+        "za": "ざ",
+        "ji": "じ",
+        "zu": "ず",
+        "ze": "ぜ",
+        "zo": "ぞ",
+
+        "da": "だ",
+        "di": "ぢ",
+        "du": "づ",
+        "de": "で",
+        "do": "ど",
+
+        "ba": "ば",
+        "bi": "び",
+        "bu": "ぶ",
+        "be": "べ",
+        "bo": "ぼ",
+
+        "pa": "ぱ",
+        "pi": "ぴ",
+        "pu": "ぷ",
+        "pe": "ぺ",
+        "po": "ぽ",
+
+        "kya": "きゃ",
+        "kyu": "きゅ",
+        "kyo": "きょ",
+
+        "gya": "ぎゃ",
+        "gyu": "ぎゅ",
+        "gyo": "ぎょ",
+
+        "nya": "にゃ",
+        "nyu": "にゅ",
+        "nyo": "にょ",
+
+        "hya": "ひゃ",
+        "hyu": "ひゅ",
+        "hyo": "ひょ",
+
+        "bya": "びゃ",
+        "byu": "びゅ",
+        "byo": "びょ",
+
+        "pya": "ぴゃ",
+        "pyu": "ぴゅ",
+        "pyo": "ぴょ",
+
+        "mya": "みゃ",
+        "myu": "みゅ",
+        "myo": "みょ",
+
+        "rya": "りゃ",
+        "ryu": "りゅ",
+        "ryo": "りょ",
+
+        "sha": "しゃ",
+        "shu": "しゅ",
+        "she": "しぇ",
+        "sho": "しょ",
+
+        "ja": "じゃ",
+        "ju": "じゅ",
+        "je": "じぇ",
+        "jo": "じょ",
+
+        "cha": "ちゃ",
+        "chu": "ちゅ",
+        "che": "ちぇ",
+        "cho": "ちょ",
+    };
+
     var homeButton = document.getElementById("review-home");
     var toTranslateElement = document.getElementById("review-to-translate");
     var translationInputElement = document.getElementById("review-translation");
@@ -14,6 +149,7 @@
     var visibleEntryIndex = 0;
 
     homeButton.onclick = srs.setScreenMain;
+    translationInputElement.oninput = transformInput;
     submitButton.onclick = submit;
     nextButton.onclick = refreshEntry;
 
@@ -59,6 +195,45 @@
 
         translationInputElement.value = "";
         translationInputElement.style.backgroundColor = "";
+    }
+
+    function transformInput() {
+        var visibleEntry = sessionEntries[visibleEntryIndex];
+        if (visibleEntry.type !== ENTRY_TYPE_E_TO_J) {
+            return;
+        }
+
+        var originalInput = translationInputElement.value;
+        var transformedInput = "";
+
+        function tryTransform(keyLength) {
+            var key = originalInput.substring(0, keyLength);
+            var value = INPUT_TRANSFORMATION_DICTIONARY[key];
+            if (value) {
+                transformedInput += value;
+                originalInput = originalInput.substring(keyLength);
+                return true;
+            }
+
+            return false;
+        }
+
+        while (originalInput.length > 0) {
+            var success = false;
+            for (var i = 1; i <= 3 && i <= originalInput.length; i++) {
+                success = tryTransform(i);
+                if (success) {
+                    continue;
+                }
+            }
+
+            if (!success) {
+                transformedInput += originalInput[0];
+                originalInput = originalInput.substring(1);
+            }
+        }
+
+        translationInputElement.value = transformedInput;
     }
 
     function submit() {
